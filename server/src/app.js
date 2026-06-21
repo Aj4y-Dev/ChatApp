97f7d63d4f8dc2369abdeea94e76b740fac83ccc
Ajay4145 <ajaytamang4145@gmail.com>
2026-06-21 07:39:28 +0545

    [+] cors implemented

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import {clerkMiddleware} from "@clerk/express";
 
@@ -11,7 +12,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    Credential: true
+}));
 
 app.use(clerkMiddleware());
 
