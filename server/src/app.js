f1082c3c7e5c209a294bc6f2efcbfd9de08cc242
Ajay4145 <ajaytamang4145@gmail.com>
2026-06-22 15:53:42 +0545

    refactor: update health check endpoint and serve static files from public directory

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 import {clerkMiddleware} from "@clerk/express";
 
@@ -11,6 +13,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+const publicDir = path.join(process.cwd(), "public"); //Gets the full path of the public folder. like C:\project\public
+
 app.use(express.json());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
@@ -19,7 +23,21 @@ app.use(cors({
 
 app.use(clerkMiddleware());
 
-app.get("/", (req, res) => res.send("Api is working"));
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        ok : true
+    })
+});
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) { //Check if the public folder exist
+  app.use(express.static(publicDir)); //Makes files inside public accessible from the browser.
+
+  app.get("/{*any}", (req, res, next) => { //For any route that Express doesn't know about (/about, /profile, etc.), send index.html.
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(port, () => {
     connectDb();
