963fae6e2dda9dc43c400847ff222c758d79ea6d
Ajay4145 <ajaytamang4145@gmail.com>
2026-06-23 12:02:46 +0545

    implement message retrieval and sending functionality with media upload support

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,12 +2,17 @@ import express from "express";
 import {
   getUsersForSidebar,
   getConversationForSidebar,
+  getmessages,
+  sendMessage,
 } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/conversation", protectRoute, getConversationForSidebar);
+router.get("/:id", protectRoute, getmessages);
+router.post("/send:id", protectRoute, upload.single("media"), sendMessage); //todo: show this in frontend
 
 export default router;
